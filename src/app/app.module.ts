a675d1b7d850d9dfb283a48abc103d912d456613
Trisith <trisith_r@cmu.ac.th>
2018-02-11 20:00:01 +0700

    complete checkout test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,19 +17,20 @@ import { firebaseConfig } from './../environments/firebase.config';
 import { AppRoutingModule } from './app-routing.module';
 
 import { NavbarComponent } from './header/navbar/navbar.component';
-// auth
+// Auth
 import { AuthService } from './auth/shared/auth.service';
 import { AuthGuard } from './auth/shared/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component'
 
 import { HomeComponent } from './home/home.component';
-// Decisiontree
+// Decision tree
 import { SearchbloodComponent } from './patient/searchblood/searchblood.component';
 import { DecisionFormComponent } from './decisiontree/decision-form/decision-form.component';
 import { DecisionListComponent } from './decisiontree/decision-list/decision-list.component';
 import { DecisionHomeComponent } from './decisiontree/decision-home/decision-home.component';
 import { DecisiontreeService } from './decisiontree/shared/decisiontree.service';
+
 // Genotype
 import { GenotypeHomeComponent } from './genediagnosis/genotype-home/genotype-home.component';
 import { MutationDetectionService } from './genediagnosis/shared/mutation-detection.service';
@@ -71,3 +72,4 @@ import { MutationDetectionService } from './genediagnosis/shared/mutation-detect
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+asdasd
