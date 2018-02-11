7e546dda0cebfadfe9f5e27e470d2b762276888e
Trisith <trisith_r@cmu.ac.th>
2018-02-11 13:16:38 +0700

    create genotype component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -13,23 +13,25 @@ import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { firebaseConfig } from './../environments/firebase.config';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { NavbarComponent } from './header/navbar/navbar.component';
-//auth
+// auth
 import { AuthService } from './auth/shared/auth.service';
 import { AuthGuard } from './auth/shared/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component'
 
 import { HomeComponent } from './home/home.component';
-//Decisiontree
+// Decisiontree
 import { SearchbloodComponent } from './patient/searchblood/searchblood.component';
 import { DecisionFormComponent } from './decisiontree/decision-form/decision-form.component';
 import { DecisionListComponent } from './decisiontree/decision-list/decision-list.component';
 import { DecisionHomeComponent } from './decisiontree/decision-home/decision-home.component';
 import { DecisiontreeService } from './decisiontree/shared/decisiontree.service';
-
+// Genotype
+import { GenotypeHomeComponent } from './genediagnosis/genotype-home/genotype-home.component';
+import { MutationDetectionService } from './genediagnosis/shared/mutation-detection.service';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import { DecisiontreeService } from './decisiontree/shared/decisiontree.service'
     SearchbloodComponent,
     DecisionFormComponent,
     DecisionListComponent,
-    DecisionHomeComponent
+    DecisionHomeComponent,
+    GenotypeHomeComponent
   ],
   imports: [
     BrowserModule,
@@ -59,7 +62,8 @@ import { DecisiontreeService } from './decisiontree/shared/decisiontree.service'
     AuthService,
     AuthGuard,
     AngularFireDatabase,
-    DecisiontreeService
+    DecisiontreeService,
+    MutationDetectionService
 
   ],
   bootstrap: [AppComponent]
---
+++ src/app/genediagnosis/genotype-home/genotype-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genotype-home',
+  templateUrl: './genotype-home.component.html',
+  styleUrls: ['./genotype-home.component.css']
+})
+export class GenotypeHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
