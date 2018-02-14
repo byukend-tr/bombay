6967f028ac19063a38961c2c1fc6068a93b992be
Trisith <trisith_r@cmu.ac.th>
2018-02-14 17:15:44 +0700

    update algo

--- src/app/genediagnosis/shared/mutation-detection.service.ts
+++ src/app/genediagnosis/shared/mutation-detection.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class MutationDetectionService {
@@ -9,83 +10,121 @@ export class MutationDetectionService {
   lcs_backtrack: Array<any>;
   edit_val: number;
   lcs_val: number;
-  mutation: string;
+  mutation: Array<string>;
+  start_at: number;
+  stop_at: number;
+
   // tslint:disable-next-line:max-line-length
-  s1 = 'ATGTGGCTCCGGAGCCATCGTCAGCTCTGCCTGGCCTTCCTGCTAGTCTGTGTCCTCTCTGTAATCTTCTTCCTCCATATCCATCAAGACAGCTTTCCACATGGCCTAGGCCTGTCGATCCTGTGTCCAGACCGCCGCCTGGTGACACCCCCAGTGGCCATCTTCTGCCTGCCGGGTACTGCGATGGGCCCCAACGCCTCCTCTTCCTGTCCCCAGCACCCTGCTTCCCTCTCCGGCACCTGGACTGTCTACCCCAATGGCCGGTTTGGTAATCAGATGGGACAGTATGCCACGCTGCTGGCTCTGGCCCAGCTCAACGGCCGCCGGGCCTTTATCCTGCCTGCCATGCATGCCGCCCTGGCCCCGGTATTCCGCATCACCCTGCCCGTGCTGGCCCCAGAAGTGGACAGC';
-  s2 = '1';
+  s1 = 'ATGTGGCTCCGGAGCCATCGTCAGCTCTGCCTGGCCTTCCTGCTAGTCTGTGTCCTCTCTGTAATCTTCTTCCTCCATATCCATCAAGACAGCTTTCCACATGGCCTAGGCCTGTCGATCCTGTGTCCAGACCGCCGCCTGGTGACACCCCCAGTGGCCATCTTCTGCCTGCCGGGTACTGCGATGGGCCCCAACGCCTCCTCTTCCTGTCCCCAGCACCCTGCTTCCCTCTCCGGCACCTGGACTGTCTACCCCAATGGCCGGTTTGGTAATCAGATGGGACAGTATGCCACGCTGCTGGCTCTGGCCCAGCTCAACGGCCGCCGGGCCTTTATCCTGCCTGCCATGCATGCCGCCCTGGCCCCGGTATTCCGCATCACCCTGCCCGTGCTGGCCCCAGAAGTGGACAGCCGCACGCCGTGGCGGGAGCTGCAGCTTCACGACTGGATGTCGGAGGAGTACGCGGACTTGAGAGATCCTTTCCTGAAGCTCTCTGGCTTCCCCTGCTCTTGGACTTTCTTCCACCATCTCCGGGAACAGATCCGCAGAGAGTTCACCCTGCACGACCACCTTCGGGAAGAGGCGCAGAGTGTGCTGGGTCAGCTCCGCCTGGGCCGCACAGGGGACCGCCCGCGCACCTTTGTCGGCGTCCACGTGCGCCGTGGGGACTATCTGCAGGTTATGCCTCAGCGCTGGAAGGGTGTGGTGGGCGACAGCGCCTACCTCCGGCAGGCCATGGACTGGTTCCGGGCACGGCACGAAGCCCCCGTTTTCGTGGTCACCAGCAACGGCATGGAGTGGTGTAAAGAAAACATCGACACCTCCCAGGGCGATGTGACGTTTGCTGGCGATGGACAGGAGGCTACACCGTGGAAAGACTTTGCCCTGCTCACACAGTGCAACCACACCATTATGACCATTGGCACCTTCGGCTTCTGGGCTGCCTACCTGGCTGGCGGAGACACTGTCTACCTGGCCAACTTCACCCTGCCAGACTCTGAGTTCCTGAAGATCTTTAAGCCGGAGGCGGCCTTCCTGCCCGAGTGGGTGGGCATTAATGCAGACTTGTCTCCACTCTGGACATTGGCTAAGCCTTGA';
+  s2: string;
 
   constructor(private http: Http) {
-    this.s1 = 'CGGACCTTTA';
-    this.s2 = 'CGGGCCTTTA';
-    for (let i = 3; i <= 57; i++) {
-      this.http.get('assets/testcase0' + i + '.txt').subscribe(data => {
-        console.log('ans:' + data.text().split('\n')[0]);
-        console.log(data.text().split('\n')[0] === this.mutation);
-      });
-      break; // do just specific case
-    }
+  }
+  testWithFile(PATH_TO_FILE) {
+    this.http.get(PATH_TO_FILE).subscribe(async data => {
+      console.log(PATH_TO_FILE);
+      this.s2 = await data.text().split('\n')[1];
+      console.log(this.s2);
+      this.__findMutate();
+      console.log('ans: ' + data.text().split('\n')[0] + ', found: ' + this.mutation[0]);
+      if (data.text().split('\n')[0] === this.mutation[0]) {
+        console.log('Correct!');
+      } else {
+        console.log('Incorrect!');
+      }
+      // this.mutation = [];
+    });
   }
 
   initService() {
-    this.edit_val = this.__editDistance(this.s1, this.s2);
-    this.lcs_val = this.__lcs(this.s1, this.s2);
-    console.log('EDIT = ' + this.edit_val);
-    console.log(this.edit);
+    this.s2 = 'ATCTGGCTCCGGAGCCATCGTCAGCTCTGCCTGGCCTTCCTGCTAGTCTGTGTCCTCTCTGTAATCTTCTTCCTCCATATCCATCAAGACAGCTTTCCACATGGCCTAGGCCTGTCGATCCTGTGTCCAGACCGCCGCCTGGTGACACCCCCAGTGGCCATCTTCTGCCTGCCGGGTACTGCGATGGGCCCCAACGCCTCCTCTTCCTGTCCCCAGCACCCTGCTTCCCTCTCCGGCACCTGGACTGTCTACCCCAATGGCCGGTTTGGTAATCAGATGGGACAGTATGCCACGCTGCTGGCTCTGGCCCAGCTCAACGGCCGCCGGGCCTTTATCCTGCCTGCCATGCATGCCGCCCTGGCCCCGGTATTCCGCATCACCCTGCCCGTGCTGGCCCCAGAAGTGGACAGCCGCACGCCGTGGCGGGAGCTGCAGCTTCACGACTGGATGTCGGAGGAGTACGCGGACTTGAGAGATCCTTTCCTGAAGCTCTCTGGCTTCCCCTGCTCTTGGACTTTCTTCCACCATCTCCGGGAACAGATCCGCAGAGAGTTCACCCTGCACGACCACCTTCGGGAAGAGGCGCAGAGTGTGCTGGGTCAGCTCCGCCTGGGCCGCACAGGGGACCGCCCGCGCACCTTTGTCGGCGTCCACGTGCGCCGTGGGGACTATCTGCAGGTTATGCCTCAGCGCTGGAAGGGTGTGGTGGGCGACAGCGCCTACCTCCGGCAGGCCATGGACTGGTTCCGGGCACGGCACGAAGCCCCCGTTTTCGTGGTCACCAGCAACGGCATGGAGTGGTGTAAAGAAAACATCGACACCTCCCAGGGCGATGTGACGTTTGCTGGCGATGGACAGGAGGCTACACCGTGGAAAGACTTTGCCCTGCTCACACAGTGCAACCACACCATTATGACCATTGGCACCTTCGGCTTCTGGGCTGCCTACCTGGCTGGCGGAGACACTGTCTACCTGGCCAACTTCACCCTGCCAGACTCTGAGTTCCTGAAGATCTTTAAGCCGGAGGCGGCCTTCCTGCCCGAGTGGGTGGGCATTAATGCAGACTTGTCTCCACTCTGGACATTGGCTAAGCCTCGA';
+    // console.log('EDIT = ' + this.edit_val);
+    // console.log(this.edit);
+
+    // console.log(this.lcs.length);
+    this.__findMutate();
     console.log(this.lcs);
     console.log(this.lcs_backtrack);
-    console.log(this.lcs.length);
-    this.__findMutate();
+
+    // let path;
+    // for (let i = 1; i <= 56; i++) {
+    //   path = 'assets/testcase' + ('0' + i).slice(-2) + '.txt';
+    //   this.testWithFile(path);
+    //   console.log(path);
+    // }
   }
 
   __findMutate() {
+    // console.log(this.s1, this.s2);
+    this.edit_val = this.__editDistance(this.s1, this.s2);
+    this.lcs_val = this.__lcs(this.s1, this.s2);
+    // console.log(this.edit_val, this.lcs_val);
     let i = this.lcs.length - 1;
     let j = this.lcs[0].length - 1;
+    console.log(this.lcs, i, j);
     const lcs_res = this.lcs[i][j];
     let mutate_pos = this.s1.length - 1;
+    this.start_at = -1;
+    this.stop_at = -1;
 
-    // S2 longer -> ignore
+    // S1 longer -> ignore
+    const i0 = i;
     while (this.lcs[i][j] === lcs_res) {
       i -= 1;
     }
     i += 1;
-    // S1 longer -> ignore
+    if (i !== i0) { // if i is changed >> s1's tail is longer
+      this.stop_at = i; // end point of s2 in s1
+    } else {
+      this.stop_at = i0;
+    }
+    // S2 longer -> ignore
     while (this.lcs[i][j] === lcs_res) {
       j -= 1;
       mutate_pos -= 1;
     }
     j += 1;
     mutate_pos += 1;
-    this.mutation = '';
-    // console.log(i, j)
+    this.mutation = new Array<string>();
     while (i > 0 && j > 0) {
-      if (this.lcs_backtrack[i][j] === 'U') {
-        if (this.lcs_backtrack[i - 1][j] === 'L') {
-          this.mutation += (mutate_pos + 1) + this.s1[mutate_pos] + '>' + this.s2[i - 1];
-          j -= 1;
-          mutate_pos -= 1;
-        } else if (this.lcs_backtrack[i][j] === 'C') {
-          j -= 1;
-        } else {
-          this.mutation += 'Deletion ' + this.s2[i - 1] + ' at ' + (mutate_pos + 1) + ',' + (mutate_pos + 2) + '<br/>';
-        }
+      while (this.lcs_backtrack[i][j] === 'C') {
         i -= 1;
-      } else if (this.lcs_backtrack[i][j] === 'L') {
-        if (this.lcs_backtrack[i][j - 1] === 'C') {
-          this.mutation += (mutate_pos + 1) + this.s1[mutate_pos] + '>' + this.s2[i - 1];
-          i -= 1;
-        } else {
-          this.mutation += 'Insertion ' + this.s2[mutate_pos] + ' at ' + (mutate_pos + 1) + '<br/>';
-          mutate_pos -= 1;
-        }
         j -= 1;
-
-      } else if (this.lcs_backtrack[i][j] === 'C') {
         mutate_pos -= 1;
-        i -= 1;
-        j -= 1;
+      }
+      if (i === 0 || j === 0) {
+        break;
+      }
+      if (this.lcs_backtrack[i][j] === 'U') {
+        // tslint:disable-next-line:max-line-length
+        if (this.lcs[i - 1][j - 1] === this.lcs[i][j]) { // Mutation case , try to get back to normal line
+          this.mutation.push((mutate_pos + 1) + this.s1[i - 1] + '>' + this.s2[j - 1]);
+          i -= 1;
+          j -= 1;
+          mutate_pos -= 1;
+        } else {
+          while (this.lcs_backtrack[i][j] === 'U') {
+            this.mutation.push(mutate_pos + 'del' + this.s1[i - 1]);
+            i -= 1;
+            mutate_pos -= 1;
+          }
+        }
+      } else if (this.lcs_backtrack[i][j] === 'L') {
+        // tslint:disable-next-line:max-line-length
+        if (this.lcs[i - 1][j - 1] === this.lcs[i][j]) { // Mutation case , try to get back to normal line
+          this.mutation.push((mutate_pos + 1) + this.s1[i - 1] + '>' + this.s2[j - 1]);
+          i -= 1;
+          j -= 1;
+          mutate_pos -= 1;
+        } else {
+          while (this.lcs_backtrack[i][j] === 'L') {
+            this.mutation.push(mutate_pos + '_' + (mutate_pos + 1) + 'ins' + this.s2[j - 1]);
+            j -= 1;
+          }
+        }
       }
     }
-    console.log(this.mutation);
+    console.log(this.mutation[0]);
   }
 
   __editDistance(s1, s2) {
@@ -126,17 +165,17 @@ export class MutationDetectionService {
             backtrack[i][j] = 'L';
           } else if (j === 0) {
             backtrack[i][j] = 'U';
-          } else {
-
           }
         } else if (s1[i - 1] === s2[j - 1]) {
           lcs[i][j] = lcs[i - 1][j - 1] + 1;
           // mark cross decision
-          backtrack[i][j] = 'C';
+          if (lcs[i][j] === lcs[i - 1][j - 1] + 1) {
+            backtrack[i][j] = 'C';
+          }
         } else {
           lcs[i][j] = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
           // mark up/left decision
-          if (lcs[i - 1][j] > lcs[i][j - 1]) { // choose from up
+          if (lcs[i - 1][j] >= lcs[i][j - 1]) { // choose from up
             backtrack[i][j] = 'U';
           } else { // choose from left
             backtrack[i][j] = 'L';
